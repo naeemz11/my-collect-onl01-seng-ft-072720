5a7203d708b79505472b213561d89caca1bc163c
Zannatul Naeem <mollahnaeem@gmail.com>
2020-07-31 19:05:46 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 def my_collect(students)
   empty_array= []
   counter= 0 
@@ -8,15 +8,7 @@ def my_collect(students)
   counter += 1 
    end 
    empty_array
-=======
-def my_collect(empty_array)
-  empty_array= []
-  counter= 0
-  my_collect(empty_array) do |x|
-    counter +=1 
-  end 
->>>>>>> cbb3ab22c9654f3281ac9633d32b8798b33ac3a9
-end 
-  
+end
+my_collect(students) {|i|
   
 
