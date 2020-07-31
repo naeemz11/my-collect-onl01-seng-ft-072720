cdc3e3ba39ca343563c1dde35ee9a725b2a61fa0
Zannatul Naeem <mollahnaeem@gmail.com>
2020-07-31 19:00:29 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
+def my_collect(students)
+  empty_array= []
+  counter= 0 
+  
+  while counter < students.length
+  empty_array << yield(students[counter])
+  counter += 1 
+   end 
+   empty_array
+end 
+  
+  
 
